a1979487d71931913389452173a724ea8359688b
BrandonLeonCastro <32971838+BrandonLeonCastro@users.noreply.github.com>
2018-03-05 14:08:07 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ function escribir(numero){
 var pantalla = document.getElementById('display')
 pantalla.innerHTML = numero
 }
+
+function engrandecer(rr) {
+document.getElementById(rr).setAttribute("style", "transform:scale(0.95,0.95)")
+}
+
+function reducir(rr) {
+document.getElementById(rr).setAttribute("style", "transform:scale(1,1)")
+}
