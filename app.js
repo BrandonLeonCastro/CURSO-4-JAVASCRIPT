a88bc8e5d33cf14777b8f3786957c7b6098986e3
BrandonLeonCastro <32971838+BrandonLeonCastro@users.noreply.github.com>
2018-03-05 12:50:44 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var num1 = document.getElementById('1')
+num1.addEventListener('click',function(){
+escribir1(1)
+})
+
+function escribir(numero){
+var pantalla = getElementById('display')
+pantalla.textcontent = numero
+}
