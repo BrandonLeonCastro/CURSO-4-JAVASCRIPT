60fdbf996421d4000972ca83d7724ab10ec1c66f
BrandonLeonCastro <32971838+BrandonLeonCastro@users.noreply.github.com>
2018-03-05 14:38:00 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var num1 = document.getElementById('1')
+var num1 = document.getElementById('')
 num1.addEventListener('click',function(){
-escribir(1)
+escribir()
 })
 
 function escribir(numero){
 var pantalla = document.getElementById('display')
-pantalla.innerHTML = numero
-}
-
-function engrandecer(rr) {
-document.getElementById(rr).setAttribute("style", "transform:scale(0.95,0.95)")
+pantalla.innerHTML += numero
 }
 
 function reducir(rr) {
+document.getElementById(rr).setAttribute("style", "transform:scale(0.95,0.95)")
+}
+
+function engrandecer(rr) {
 document.getElementById(rr).setAttribute("style", "transform:scale(1,1)")
 }
