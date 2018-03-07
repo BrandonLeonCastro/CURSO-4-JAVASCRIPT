f1ec72b29daaba98b57e1e3e429bb468df80076a
BrandonLeonCastro <32971838+BrandonLeonCastro@users.noreply.github.com>
2018-03-06 20:45:24 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var pantalla = document.getElementById('display')
+var valora;
+var valorb;
+var operacion;
 
 function escribir(numero){
-   if (pantalla.innerHTML == 0) {
-       pantalla.innerHTML = ""
+  if (pantalla.innerHTML == 0) {
+      pantalla.innerHTML = ""
+  }
+
+var aux= pantalla.innerHTML ;
+console.log(aux);
+if(aux.indexOf(".") != (-1)){
+  if(numero!="."){
+    pantalla.innerHTML += numero
+    pantalla.innerHTML = pantalla.innerHTML.substring(0, 9)
    }
+ }
+else{
    pantalla.innerHTML += numero
    pantalla.innerHTML = pantalla.innerHTML.substring(0, 8)
+  }
 }
 
 function reducir(rr) {
@@ -18,4 +32,72 @@ document.getElementById(rr).setAttribute("style", "transform:scale(1,1)")
 
 function borrar(numero){
   pantalla.innerHTML = 0
+}
+
+function init(){
+  var suma = document.getElementById('mas');
+  var resta = document.getElementById('menos');
+  var multiplicacion = document.getElementById('por');
+  var division = document.getElementById('dividido');
+  var igual = document.getElementById('igual');
+  var on = document.getElementById('on');
+  var signo = document.getElementById('sign');
+  var pantalla = document.getElementById('display');
+
+  suma.onclick = function(num){
+    valora = display.textContent;
+    operacion = "+"
+    limpiar();
+  }
+  resta.onclick = function(num){
+    valora = display.textContent;
+    operacion = "-"
+    limpiar();
+  }
+  multiplicacion.onclick = function(num){
+    valora = display.textContent;
+    operacion = "*"
+    limpiar();
+  }
+  division.onclick = function(num){
+    valora = display.textContent;
+    operacion = "/"
+    limpiar();
+  }
+  igual.onclick = function(num){
+    valorb = display.textContent;
+    resolver();
+  }
+}
+
+function limpiar(){
+  display.textContent = "";
+}
+
+function resetear(){
+  display.textContent = "0";
+  valora = 0;
+  valorb = 0;
+  operacion = "";
+}
+
+function resolver(){
+  var res = 0;
+  switch(operacion){
+    case "+":
+         res = parseFloat(valora) + parseFloat(valorb);
+         break;
+    case "-":
+         res = parseFloat(valora) - parseFloat(valorb);
+         break;
+    case "*":
+         res = parseFloat(valora) * parseFloat(valorb);
+         break;
+    case "/":
+         res = parseFloat(valora) / parseFloat(valorb);
+         break;
+
+  }
+  resetear();
+  display.textContent = res;
 }
